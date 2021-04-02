f3bde65693485928c13e5253c1202f7f8d75c2bc
John Pittman <john.pittman@outdoorsy.co>
2021-04-01 22:56:47 -0700

    chore: husky ci test

--- husky.config.js
+++ husky.config.js
@@ -4,7 +4,7 @@ module.exports = {
   hooks: {
     'post-checkout': `if [[ $HUSKY_GIT_PARAMS =~ 1$ ]]; then ${runPackageLock}; fi`,
     'post-merge': runPackageLock,
-    'post-rebase': runPackageLock,
-    'pre-commit': 'npm test & npm run lint:staged'
+    'post-rebase': runPackageLock
+    // 'pre-commit': 'npm test & npm run lint:staged'
   }
 };
