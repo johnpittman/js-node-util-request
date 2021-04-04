179be6894788385555e5c638a6cbcd434f1b2bc1
John Pittman <john.pittman@outdoorsy.co>
2021-04-04 01:42:06 -0700

    fix: setting any content-type skips sendJSON

--- src/middleware/sendJSON.ts
+++ src/middleware/sendJSON.ts
@@ -4,11 +4,12 @@ let sendJSON: RequestMiddleware = (params) => {
   let { fetchOpts } = params;
 
   if (fetchOpts) {
-    let headers: any = (fetchOpts.headers = fetchOpts.headers || {});
-    let hasContentTypeHeader = headers['content-type'] || headers['Content-Type'] || fetchOpts.body instanceof FormData;
+    let headers = (fetchOpts.headers = fetchOpts.headers || {});
+    let contentType = headers['content-type'] || headers['Content-Type'];
+    let isNotJSON = contentType && (contentType !== 'application/json' || fetchOpts.body instanceof FormData);
 
     // Allow user to send what they want
-    if (fetchOpts.body !== undefined && !hasContentTypeHeader) {
+    if (fetchOpts.body !== undefined && !isNotJSON) {
       headers['content-type'] = 'application/json';
 
       fetchOpts.body = JSON.stringify(fetchOpts.body);
