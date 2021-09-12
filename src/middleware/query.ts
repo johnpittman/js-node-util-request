ff381d691088b2496e8f190b06d3dbe4e31410a0
John Pittman <john.pittman@outdoorsy.co>
2021-09-11 19:38:38 -0700

    docs: comments

--- src/middleware/query.ts
+++ src/middleware/query.ts
@@ -3,7 +3,7 @@ import queryString from 'query-string';
 import { RequestMiddlewareParams } from '>/request';
 
 let query = (params: RequestMiddlewareParams) => {
-  // Cannot set when fetchInput is a Request
+  // Cannot set when fetchInput is a Request object
   if (typeof params.fetchInput === 'string' && params.userOpts?.query) {
     let queryStr = queryString.stringify(params.userOpts.query, params.userOpts.queryOpts);
 
